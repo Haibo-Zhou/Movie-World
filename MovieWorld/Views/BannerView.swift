c2ce9bbfc4008208dc25abdb0ce512b233d05124
Chuck Zhou <chuckzhb@hotmail.com>
2020-04-29 11:51:39 +0800

    1) Replace test Ad unit id with real one.

--- MovieWorld/Views/BannerView.swift
+++ MovieWorld/Views/BannerView.swift
@@ -15,7 +15,7 @@ struct BannerView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         let view = GADBannerView(adSize: kGADAdSizeBanner)
         let viewController = UIViewController()
-        view.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        view.adUnitID = "ca-app-pub-7792718102871712/7058930154"
         view.rootViewController = viewController
         viewController.view.addSubview(view)
         viewController.view.frame = CGRect(origin: .zero, size: kGADAdSizeBanner.size)
