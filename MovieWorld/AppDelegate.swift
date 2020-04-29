18bd4c137b3659815d7c7c8da7a5de0beec5a9c6
Chuck Zhou <chuckzhb@hotmail.com>
2020-04-29 11:58:50 +0800

    Marketing build with real Ads, version 1.0.9

--- MovieWorld/AppDelegate.swift
+++ MovieWorld/AppDelegate.swift
@@ -17,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Initialize Firebase
-        FirebaseApp.configure()
-        
         // Initialize AdMob
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
